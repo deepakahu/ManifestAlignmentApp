636e50321a069a6b6c7be7b322fac6e59367f9ee
Deepak Ahu <deepakahu@gmail.com>
2026-05-13 22:01:31 -0700

    PM2 EADDRINUSE on port 3001 (fixed in ecosystem.config.js)

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,6 +14,8 @@ module.exports = {
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
+      kill_timeout: 5000,
+      listen_timeout: 10000,
       error_file: '~/.pm2/logs/manifest-web-error.log',
       out_file: '~/.pm2/logs/manifest-web-out.log',
       log_file: '~/.pm2/logs/manifest-web-combined.log',
